1474e403563089f9a070574dd485faa13086f1ef
Ivan <mmavrin80@gmail.com>
2022-04-24 21:49:12 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿void IfPow (int arg)
+{
+    if (arg < 0)
+    {
+        Console.WriteLine("Не может являться степенью");
+        return;
+    }
+    for (int i = 0; i <= arg; i++)
+    {
+        if (Math.Pow(2, i) == arg)
+        {
+            Console.Write("Является степенью двойки");
+            return;
+        }
+        if (Math.Pow(2, i) > arg)
+        {
+            Console.Write("Не является степенью двойки");
+            return;
+        }
+    }
+}
+
+IfPow(12);

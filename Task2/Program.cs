d0dbea9bcd4f64a68d9376505ae0e34fb42de0e6
Ivan <mmavrin80@gmail.com>
2022-04-24 22:01:21 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿void Check(int arg)
+{
+    if (arg < 1)
+    {
+        Console.WriteLine("Это не натуральное число");
+        return;
+    }
+    for (int i = 2; i < arg; i++)
+    {
+        if (arg % i == 0)
+        {
+            Console.WriteLine("Это не простое число");
+            return;
+        }
+    }
+    Console.WriteLine("Это простое число");
+    return;
+}
+
+Check(0);
